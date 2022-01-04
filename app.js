bc4c7265f3864301cc2aea6ea4bf9f8c304c0def
Fliberatore88 <federico.liberatore88@gmail.com>
2022-01-04 09:12:57 -0300

    se agregó el err en el app.js

--- app.js
+++ app.js
@@ -7,9 +7,16 @@ app.use (express.static(publicPath) );
 
 app.get ('/', (req,res) => {
     res.sendFile (path.resolve(__dirname, 'views','home.html'))
+    
 })
 app.get ('/Ofertas', (req,res) => {
     res.sendFile (path.resolve(__dirname, 'views','ofertas.html'))
 })
 
-app.listen(3001, () => console.log('Servidor corriendo correctamente'))
+app.listen(3001, (err) => {
+    if (err){
+        console.log('Error', err)
+    }else {
+        console.log('Levantó el Server correctamente')
+    }
+})
